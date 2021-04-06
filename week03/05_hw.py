d7888d3090d6261439441650fe70d7f6403a303a
Rakhman Magametov <rakhmanmagametov@gmail.com>
2021-04-06 18:30:39 +0600

    Some new Home Works

--- week03/05_hw.py
+++ week03/05_hw.py
@@ -1,5 +1,13 @@
 a = input().split()
 k = int(input())
+<<<<<<< HEAD
+if k >= 0:
+    for i in range(k):
+        a[i] = a[len(a) + i -k]
+    for i in range(len(a) - 1, k-1, -1):
+        a[i] = a[i-k]
+print(a)
+=======
 if abs(k) > len(a):
     if k > 0:
         k = k % len(a)
@@ -15,3 +23,4 @@ else:
         print(int(a[i]), end = " ")
     for i in range(abs(k)):
         print(int(a[i]), end = " ")
+>>>>>>> bc84f1439b076bc96d8873efc131ba5317674cd7
